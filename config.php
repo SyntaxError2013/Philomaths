d460127321466ec9a4078d3afa7a80a9b99c1be0
Shivam <shivam.mangla.09@gmail.com>
2013-10-20 07:19:23 -0700

    Modify config.php (Ignore very small change)

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
 
 define('DB_USER', "root"); // db user
 define('DB_PASSWORD', ""); // db password (mention your db password here)
-define('DB_DATABASE', "humsafar"); // database name
+define('DB_DATABASE', "test"); // database name
 define('DB_HOST', "localhost"); // db server
 
 define("GOOGLE_API_KEY", "AIzaSyBSWVK3SxQrpJUS_ffAAJxLCknTV9vDRyU"); 
