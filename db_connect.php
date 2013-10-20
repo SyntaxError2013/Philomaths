aa2101681606d6ebfe8f0d64972281faa1e11af4
Shivam <shivam.mangla.09@gmail.com>
2013-10-20 04:40:38 -0700

    Reflect the changes in db_connect.php due to renaming of config file

--- db_connect.php
+++ db_connect.php
@@ -4,8 +4,8 @@ class DB_CONNECT {
 
     function connect() {
 	
-        require_once __DIR__ . '/db_config.php';
-        $con = mysql_connect(DB_SERVER, DB_USER, DB_PASSWORD) or die(mysql_error());
+        require_once 'config.php';
+        $con = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD) or die(mysql_error());
         $db = mysql_select_db(DB_DATABASE) or die(mysql_error()) or die(mysql_error());
 
         // returing connection cursor
