8a812cbd005dae7d51feed99aabbc27d954fe6f4
Shivam <shivam.mangla.09@gmail.com>
2013-10-20 04:37:54 -0700

    Add google_api_key

--- db_config.php
+++ db_config.php
@@ -9,5 +9,5 @@ define('DB_PASSWORD', ""); // db password (mention your db password here)
 define('DB_DATABASE', "humsafar"); // database name
 define('DB_HOST', "localhost"); // db server
 
-define("GOOGLE_API_KEY", ""); 
+define("GOOGLE_API_KEY", "AIzaSyBSWVK3SxQrpJUS_ffAAJxLCknTV9vDRyU"); 
 ?>
